8bf76c11545ddf008b7691fb9c8ca75a60f8434f
pjsmelodev <pjsm.lesi@gmail.com>
2025-02-08 18:23:55 +0000

    Added several script files

---
+++ write_file.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# imprime no terminal
+echo "Hello World!"
+
+# (cria e) guarda num ficheiro
+echo "This is some text." > output1.txt
---
+++ your_name2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Hello, $1!"
+# $1 denota o primeiro argumento recebido pelo script
+# tem de receber um nome como parâmetro
+# ./your_name2.sh Paulo
